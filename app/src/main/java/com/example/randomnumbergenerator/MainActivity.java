54524733f2ae783063d4f265765ef0a4500d7aaa
Praveen Kumar Pankaj <praveenkumarpankaj0@gmail.com>
2021-09-15 14:18:02 +0530

    Submit Button
    Implementation of submit button functionality

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +20,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void generateRandom(View view){
-        view = findViewById(R.id.initial_num);
+        EditText initial_num ,final_num;
+        TextView result;
+        String output;
+        Random random = new Random();
+
+        initial_num = (EditText)findViewById(R.id.initial_num);
+        final_num = (EditText)findViewById(R.id.final_num);
+        result = (TextView)findViewById(R.id.textView4);
+
+        int min;
+        try{
+            min = Integer.parseInt(initial_num.getText().toString());
+        }
+        catch (Exception e){
+            min = 1;
+        }
+
+        int max;
+        try {
+            max = Integer.parseInt(final_num.getText().toString());
+        }
+        catch (Exception e){
+            max = 100;
+        }
+
+        output = "" + (random.nextInt(max - min + 1) + min);
+        result.setText(output);
+        result.setVisibility(View.VISIBLE);
     }
 
 }
